1e37eb48e7aa51daa55a770688935fc006610953
Mouka <82287405+Moukac@users.noreply.github.com>
2022-03-18 19:46:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+const url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
     jokeContainer.classList.remove("fade");
